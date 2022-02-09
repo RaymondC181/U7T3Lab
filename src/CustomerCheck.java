29254b3731f4ccb77a129aa26e83999fc0e826f6
Raymond <raymondc181@nycstudents.net>
2022-02-08 19:41:40 -0500

    customerCheck completed

--- src/CustomerCheck.java
+++ src/CustomerCheck.java
@@ -30,12 +30,41 @@ public class CustomerCheck
      *  returns false otherwise, as described in part (b) */
     public boolean couponApplies()
     {
-        
+        for(MenuItem item : check)
+        {
+            if(item.isDailySpecial())
+            {
+                return false;
+            }
+        }
+        if(totalPrices()<40)
+        {
+            return false;
+        }
+        return true;
     }
 
     /** Calculates the final cost of this check, as described in part (c) */
     public double calculateCheck()
     {
-        /* to be implemented in part (c) */
+        int customerCount = 0;
+        double discount = 0;
+        double tip = 0;
+
+        double finalCost = totalPrices();
+        for(MenuItem item: check) {
+            if (item.isEntree()) {
+                customerCount++;
+            }
+        }
+        if(couponApplies())
+        {
+            discount = finalCost*0.25;
+        }
+        if(customerCount >= 6)
+        {
+            tip = finalCost*0.2;
+        }
+        return finalCost - discount + tip;
     }
 }
